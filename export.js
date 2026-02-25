b94af85f0c3d05242df5dff9707c800f584dc7c6
Premkumar Savale <premsavle112@gmail.com>
2026-02-25 11:18:18 +0530

    Added Some Changes

---
+++ export.js
@@ -0,0 +1,10 @@
+
+module.exports = 
+
+    {
+        FirstName: "Premkumar",
+        LastName: "Savale",
+        age: 24,
+        city: "Burhanpur",
+
+    }
---
+++ import.js
@@ -0,0 +1,8 @@
+
+const data = require("./export.js")
+
+
+console.log("FirstName is ======> ", data.FirstName);
+console.log("LastName is ======> ", data.LastName);
+console.log(" Age is ======> ", data.age);
+console.log("city is ======>", data.city);
