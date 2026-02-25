6449e240148b3d13854cd49b4e40f96df2d8c8f9
Premkumar Savale <premsavle112@gmail.com>
2026-02-25 12:12:58 +0530

    Load Html file in node js

---
+++ web.js
@@ -0,0 +1,24 @@
+
+const http = require("http");
+const fs = require("fs");
+
+http.createServer((req, res) => {
+    fs.readFile("web.html", "utf-8", (err, data) => {
+
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.write("Internal Serval error");
+            res.end();
+            return;
+
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+
+        res.write(data);
+
+        res.end();
+
+
+    })
+
+}).listen(2300);
