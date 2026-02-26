1f2426aa733e916a46919900e6a2c21213405ac9
Premkumar Savale <premsavle112@gmail.com>
2026-02-26 12:20:17 +0530

    Added api integretion

--- Route_for_get_api.js
+++ Route_for_get_api.js
@@ -2,12 +2,16 @@ import express from 'express';
 import { MongoClient } from 'mongodb';
 
 const app = express();
-// app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 
 const dbName = "College";
 const url = "mongodb://localhost:27017";
 
 const client = new MongoClient(url);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 client.connect().then(() => {
 
@@ -20,10 +24,85 @@ client.connect().then(() => {
 
         res.send(data);
 
+    });
+    app.get("/ui", async (req, res) => {
 
-        res.status(500).send("Database Error");
+        const db = client.db(dbName);
+        const collection = db.collection("Student");
+
+        const data = await collection.find().toArray();
+
+        res.render('data', { data });
 
     });
+
+    app.get("/add", (req, res) => {
+
+        res.send(`
+                     
+        <form method="post" action="add-student"> 
+         
+         <input type="text"  placeholder="Enter FirstName"  name="FirstName"  />
+          <br/>
+          <br/>
+
+         <input type="text"  placeholder="Enter LastName"  name="LastName"  />
+          <br/>
+          <br/>
+
+          <input type="text"  placeholder="Enter City"  name="City"  />
+           <br/>
+           <br/>
+
+          <input type="text"  placeholder="Enter age"  name="age"  />
+            <br/>
+            <br/>
+
+ 
+            <input type="text"  placeholder="Enter role"  name="role"  />
+               <br/>
+               <br/>
+
+              <button>  Add Button  </button> 
+
+        </form>
+             
+            `)
+
+    });
+
+    app.post("/add-student", async (req, res) => {
+        //console.log(req.body);
+        const db = client.db(dbName);
+        const collection = db.collection("Student");
+        const data = await collection.insertOne(req.body);
+
+        console.log(data);
+
+        // const data = await collection.find().toArray();
+        res.send("DATA SAVED .....");
+
+    });
+
+    app.post("/add-student-api", async (req, res) => {
+
+        // console.log(req.body);
+
+        const { name, age, email } = req.body;
+
+        if (!name || !age || !email) {
+            res.send({ message: "Operation Failed", success: false })
+            return false
+        }
+        const db = client.db(dbName);
+        const collection = db.collection("Student");
+        const data = await collection.insertOne(req.body);
+
+        console.log(data);
+
+        res.send({ message: "data stored ", success: true, data: data });
+
+    })
 
 });
 
